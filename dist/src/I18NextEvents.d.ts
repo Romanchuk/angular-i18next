32a1ecbfbea406c602f5f7d1169441ba5669c5cf
f.shchudlo <fshchudlo@gmail.com>
2017-06-28 14:46:45 +0700

    chore(all): dist folder removed

--- dist/src/I18NextEvents.d.ts
+++
@@ -1,7 +0,0 @@
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-export declare class I18NextEvents {
-    initialized: BehaviorSubject<boolean>;
-    loaded: BehaviorSubject<boolean>;
-    failedLoading: BehaviorSubject<any>;
-    languageChanged: BehaviorSubject<string>;
-}
--- dist/src/I18NextModule.d.ts
+++
@@ -1,4 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export declare class I18NextModule {
-    static forRoot(): ModuleWithProviders;
-}
