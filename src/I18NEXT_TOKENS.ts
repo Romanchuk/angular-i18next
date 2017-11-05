be561b16b5a4a681a9b0d538a7633811306ceb73
Romanchuk <rezety@gmail.com>
2017-11-05 12:31:03 +0700

    Typed InjectionToken

--- src/I18NEXT_TOKENS.ts
+++ src/I18NEXT_TOKENS.ts
@@ -1,5 +1,6 @@
 import { InjectionToken } from '@angular/core';
+import { ITranslationService } from './ITranslationService';
 
-export const I18NEXT_SCOPE = new InjectionToken('I18NEXT_SCOPE');
-export const I18NEXT_NAMESPACE = new InjectionToken('I18NEXT_NAMESPACE');
-export const I18NEXT_SERVICE = new InjectionToken('I18NEXT_SERVICE');
+export const I18NEXT_SCOPE = new InjectionToken<string>('I18NEXT_SCOPE');
+export const I18NEXT_NAMESPACE = new InjectionToken<string>('I18NEXT_NAMESPACE');
+export const I18NEXT_SERVICE = new InjectionToken<ITranslationService>('I18NEXT_SERVICE');
