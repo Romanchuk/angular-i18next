f17c1716f83811e6134a6791a7dd8b21dd9cb40d
Romanchuk <rezety@gmail.com>
2018-06-04 21:04:05 +0700

    update packages: angular, rxjs

--- src/I18NextEvents.ts
+++ src/I18NextEvents.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { BehaviorSubject } from 'rxjs';
 import { ITranslationEvents } from './ITranslationEvents';
 
 export class I18NextEvents implements ITranslationEvents {
