82e11eae2fba2ba1f0219b9961933bc411b8273e
Sergey Romanchuk <rezety@gmail.com>
2020-02-18 11:03:27 +0700

    Support banner

---
+++ postinstall.js
@@ -0,0 +1,5 @@
+var BANNER = '\u001B[96mThank you for using angular-i18next (https://github.com/Romanchuk/angular-i18next). Please star the repo!\u001B[0m\n\n' +
+             '\u001B[96mThe project needs your help! Please consider supporting of angular-i18next \u001B[0m\n' +
+             '\u001B[96m>\u001B[94m Say thanks via donation: https://www.paypal.com/paypalme2/sergeyromanchuk/10USD \u001B[0m\n';
+
+console.log(BANNER);
